406b6a8b7ac4fe08f5c812d08d74fcbc56b0eb06
Inna <inna.kachinskaya13@gmail.com>
2021-12-09 22:54:57 +0200

    Add server

---
+++ server.js
@@ -0,0 +1,85 @@
+const http = require("http");
+
+const express = require("express");
+const mongoose = require("mongoose");
+const yup = require("yup");
+
+const DB_NAME = process.env.DB_NAME || "mongoose_db";
+
+mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`).catch((error) => {
+  //в этом месте можно создать логгер ошибок
+  console.log(error);
+  process.exit(1);
+});
+
+const phoneSpecsSchema = yup.object().shape({
+  cpu: yup.string().required(),
+  ram: yup.number().min(1).required(),
+  storage: yup.number().min(1).required(),
+});
+
+const PORT = process.env.PORT || 3000;
+//================================================================
+
+//создаем схему
+const phoneSchema = new mongoose.Schema({
+  brand: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => /[A-Za-z]{1,64}/.test(value),
+    },
+  },
+  model: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  specs: {
+    type: Object,
+    required: true,
+    validate: {
+      validator: (value) => phoneSpecsSchema.isValid(value),
+    },
+  },
+});
+
+//================================================================
+
+//создаем модель
+const Phone = mongoose.model("Phones", phoneSchema);
+
+//================================================================
+const app = express();
+
+app.use(express.json());
+
+app.post("/", async (req, res, next) => {
+  try {
+    const { body } = req;
+    const phone = await Phone.create(body);
+    res.status(201).send({ data: phone });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/", async (req, res, next) => {
+  try {
+    const phones = await Phone.find();
+    res.status(200).send({ data: phones });
+  } catch (error) {}
+});
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log("Server is up");
+});
